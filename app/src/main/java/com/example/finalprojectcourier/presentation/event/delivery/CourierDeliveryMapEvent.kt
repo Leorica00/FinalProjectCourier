e272aa0884d9e41e85b371155998a10db0856959
Giga Maisuradze <giga.maisuradze.1@btu.edu.ge>
2024-03-24 13:43:07 +0400

    add chat

--- app/src/main/java/com/example/finalprojectcourier/presentation/event/delivery/CourierDeliveryMapEvent.kt
+++ app/src/main/java/com/example/finalprojectcourier/presentation/event/delivery/CourierDeliveryMapEvent.kt
@@ -2,5 +2,6 @@ package com.example.finalprojectcourier.presentation.event.delivery
 
 sealed class CourierDeliveryMapEvent {
     object GetMenuUpdateEvent: CourierDeliveryMapEvent()
+    object NavigateToChat: CourierDeliveryMapEvent()
     object UpdateCourierLocationEvent: CourierDeliveryMapEvent()
 }
